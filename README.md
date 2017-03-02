8e722f9ad791a101f30f2ccfc0884184a1f8073d
Jennifer Presto <presj521@newschool.edu>
2017-03-02 12:59:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Actions on Google Silly Name Maker Sample using Node.js
+
+This is a super simple Action, built using API.AI and Node.js, to generate a
+silly name based on your lucky number and favorite color.
+
+## Setup Instructions
+
+### Pre-requisites
+ 1. API.AI account: [https://api.ai](https://api.ai)
+ 2. Google Cloud project: [https://console.cloud.google.com/project](https://console.cloud.google.com/project)
+
+See the developer guide and release notes at [https://developers.google.com/actions/](https://developers.google.com/actions/) for more details.
+
+### Steps
+ 1. Create a new agent in API.AI [https://api.ai](https://api.ai).
+ 1. Click on the project gear icon to see the project settings.
+ 1. Select "Export and Import".
+ 1. Select "Restore from zip". Follow the directions to restore.
+ 1. Select the SillyNameMaker.zip file in this repo.
+ 1. Deploy this app to your preferred hosting environment (we recommend Google App Engine).
+ 1. Set the "Fulfillment" webhook URL to the hosting URL.
+ 1. Make sure all domains are turned off.
+ 1. Enable Actions on Google in the Integrations.
+ 1. In the make_name intent, check "End Conversation" for Actions on Google.
+ 1. Provide an invocation name for the action.
+ 1. Authorize and preview the action in the [web simulator](https://developers.google.com/actions/tools/web-simulator).
+
+For more detailed information on deployment, see the [documentation](https://developers.google.com/actions/samples/).
+
+## References and How to report bugs
+* Actions on Google documentation: [https://developers.google.com/actions/](https://developers.google.com/actions/).
+* If you find any issues, please open a bug here on GitHub.
+* Questions are answered on [StackOverflow](https://stackoverflow.com/questions/tagged/actions-on-google).
+
+## How to make contributions?
+Please read and follow the steps in the CONTRIBUTING.md.
+
+## License
+See LICENSE.md.
+
+## Terms
+Your use of this sample is subject to, and by using or downloading the sample files you agree to comply with, the [Google APIs Terms of Service](https://developers.google.com/terms/).
+
+## Google+
+Actions on Google Developers Community on Google+ [https://g.co/actionsdev](https://g.co/actionsdev).
---
+++ app.js
@@ -0,0 +1,84 @@
+// Copyright 2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+let Assistant = require('actions-on-google').ApiAiAssistant;
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.use(bodyParser.json({type: 'application/json'}));
+
+const TEST_ACTION = 'test_action';
+const WELCOME_ACTION = 'welcome_action';
+const NAME_ACTION = 'make_name';
+const COLOR_ARGUMENT = 'color';
+const NUMBER_ARGUMENT = 'number';
+
+//  Save the array of welcome messages;
+//  The first one will return a special context
+let Welcomes = [];
+Welcomes.push('Hey, oh... duh... nah, I\'m not busy. Come on in!');
+Welcomes.push('Hey, I\'m Brian... I hear you\'re having trouble writing. What can I help you with\?');
+Welcomes.push('Hey, dude... I\'m Brian... are you having trouble with your writing? Hit me up... hwat are you trying to do?');
+Welcomes.push('This is the third welcome');
+
+// [START SillyNameMaker]
+app.post('/', function (req, res) {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  // Make a silly name
+  function makeName (assistant) {
+    let number = assistant.getArgument(NUMBER_ARGUMENT);
+    let color = assistant.getArgument(COLOR_ARGUMENT);
+    assistant.tell('Alright, your silly name is ' +
+      color + ' ' + number +
+      '! I hope you like it. See you next time.');
+  }
+
+  function welcome(assistant) {
+    let randomWelcome = Math.floor((Math.random() * Welcomes.length) + 1);
+    console.log('Random number for the welcome is' + randomWelcome);
+    assistant.tell(Welcomes[randomWelcome]);
+  }
+
+  function testResponse(assistant) {
+    assistant.tell('I\'m trying to get some sweet rhymes for you, and I\'m totally on the backend.');
+  }
+
+
+
+  let actionMap = new Map();
+  actionMap.set(NAME_ACTION, makeName);
+  actionMap.set(TEST_ACTION, testResponse);
+  actionMap.set(WELCOME_ACTION, welcome);
+
+  assistant.handleRequest(actionMap);
+});
+// [END SillyNameMaker]
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+  // [END server]
+}
+
+module.exports = app;
