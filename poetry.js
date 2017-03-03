fbe2d9d49d5c2652bc10d9d89816928e473febb9
Cameron Browning <cameron@potiondesign.com>
2017-03-03 10:30:31 -0500

    added max count param for return rhymes

--- poetry.js
+++ poetry.js
@@ -2,18 +2,25 @@ var request = require('request');
 
 module.exports = {
 
-    rhyme: function (_word, callback) {
+    rhyme: function (_word, _maxCount, callback) {
 
         request('http://rhymebrain.com/talk?function=getRhymes&word=' + _word, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var mRhymes = JSON.parse(body);
                 var mGoodRhymes = [];
 
-                for(var i=0;i<mRhymes.length; i++){
+                for(var i=0;i< mRhymes.length; i++){
                     if(mRhymes[i].score >= 300){
-
                         mGoodRhymes.push(mRhymes[i].word);
                     }
+                }
+                // console.log("good rhyme count: "+mGoodRhymes.length + ", but i'm requesting only " + _maxCount);
+
+
+                while(mGoodRhymes.length>_maxCount){
+                    var len = mGoodRhymes.length-1;
+                    var rand = module.exports.getRandomIntInclusive(0,len);
+                    mGoodRhymes.splice(rand,1);
                 }
 
                 callback(mGoodRhymes);
@@ -22,7 +29,12 @@ module.exports = {
 
         return 0;
 
-    }
+    },
+    getRandomIntInclusive: function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 };
 
 
